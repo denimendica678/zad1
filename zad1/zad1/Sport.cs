d5e1e9e58da14787c55157d9ff945d0c2152113a
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-10-18 08:58:42 +0200

    Add files via upload

---
+++ zad1/zad1/Sport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    public class Sport
+    {
+
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(byte brojIgraca, string naziv, bool igraLiSeLoptom)
+        {
+            BrojIgraca = brojIgraca;
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+        }
+
+        public string Naziv1 { get => Naziv; set => Naziv = value; }
+        public bool IgraLiSeLoptom1 { get => IgraLiSeLoptom; set => IgraLiSeLoptom = value; }
+        public byte BrojIgraca1 { get => BrojIgraca; set => BrojIgraca = value; }
+    }
+}
